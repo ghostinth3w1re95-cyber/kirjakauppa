666b16849923b10db8ade859b73083f1673168f4
ghostinth3w1re95-cyber <ghostinth3w1re.95@gmail.com>
2026-02-09 11:18:22 +0200

    tehty 2

--- src/main/java/oma/kirja/kauppa/web/BookController.java
+++ src/main/java/oma/kirja/kauppa/web/BookController.java
@@ -1,9 +1,10 @@
 package oma.kirja.kauppa.web;
 
+import oma.kirja.kauppa.domain.Book;
 import oma.kirja.kauppa.domain.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BookController {
@@ -30,6 +31,41 @@ public class BookController {
     public String showBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "books";
+    }
+
+    @GetMapping("/addbook")
+    public String showAddBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditBook(@PathVariable Long id, Model model) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            model.addAttribute("book", book);
+            return "edit";
+        }
+        return "redirect:/books";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editBook(@PathVariable Long id, @ModelAttribute Book book) {
+        book.setId(id);
+        bookRepository.save(book);
+        return "redirect:/books";
     }
 }
 
